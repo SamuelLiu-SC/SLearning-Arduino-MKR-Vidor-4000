2234c2c2399d96062e1b321ef0b9e02ed4f8ee7a
SamuelLiu-SC <samuelmyliu@gmail.com>
2025-10-09 14:24:31 +0100

    Update analog_input_A1.ino

--- S05_analog_intput/analog_input_A1/analog_input_A1.ino
+++ S05_analog_intput/analog_input_A1/analog_input_A1.ino
@@ -1,12 +1,14 @@
 #define LED_PIN 5
+#define analog_in_PIN A1
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(analog_in_PIN, INPUT);
 }
 
 int i;  
 
 void loop() {
-  i = analogRead(A1)/4;
+  i = analogRead(analog_in_PIN)/4;
   analogWrite(LED_PIN, i); 
 }
